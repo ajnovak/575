2767f76317cc125ee015479b2c3d34d640aac1b5
ajnovak <79161339+ajnovak@users.noreply.github.com>
2022-03-12 12:08:10 -0600

    update

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+=======
+var map = L.map('map').setView([100, -40], );
+>>>>>>> Stashed changes
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,4 +12,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic2VzYWVybyIsImEiOiJjbDBuNGhpcGIxY3IzM2pyNW96czFuMG84In0.De8p1x2EIr7DtypLDwRLrQ'
+<<<<<<< Updated upstream
 }).addTo(map);
+=======
+}).addTo(map);
+
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+L.geoJSON(myLines).addTo(map);
+>>>>>>> Stashed changes
