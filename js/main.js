6d5c1f8fbc7e381e0e1db98ea5a86c6b28c826dd
ajnovak <79161339+ajnovak@users.noreply.github.com>
2022-03-11 18:55:15 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+function initialize(){
+    mainmap();
+}
+
+function mainmap(){
+    var map = L.map('map').setView([51.505, -0.09], 13);
+}
+
+
+window.onload = initialize();
--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -1,4 +1,11 @@
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2VzYWVybyIsImEiOiJjbDBtemlzc3QxYmY4M29sNGFieXRwbzRoIn0.vI2msNxuqSqC2v5Tqt6RYg'
+}).addTo(map);
